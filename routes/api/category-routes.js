8d83bb41a58c75419a37765ace854a999d9fa001
Juanjohn11 <elmatanaco1144@gmail.com>
2022-03-21 17:08:02 -0700

    never pushed my code upstream

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -5,12 +5,33 @@ const { Category, Product } = require('../../models');
 
 router.get('/', (req, res) => {
   // find all categories
+  Category.findAll({
+    include: [Product],
+  })
   // be sure to include its associated Products
+  .then((category)=> {
+    res.json(category)
+  })
+  .catch((err)=> {
+    res.json(500).json(err)
+  });
 });
 
 router.get('/:id', (req, res) => {
   // find one category by its `id` value
+  Category.findOne({
+    where: {
+      id: req.params.id,
+    },
+    include: [Product]
+  })
   // be sure to include its associated Products
+  .then((category)=> {
+    res.json(category)
+  })
+  .catch((err)=> {
+    res.json(500).json(err)
+  });
 });
 
 router.post('/', (req, res) => {
